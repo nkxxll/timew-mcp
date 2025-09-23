fd02e063635067f9d31062c3a4f4a714461601b9
nkxxll <niki.nice1203@gmail.com>
2025-09-23 15:53:51 +0200

    plug handler to stdin stdout

--- bin/main.ml
+++ bin/main.ml
@@ -1,3 +1,52 @@
 open Core
+open Timew_mcp
 
-let () = Timew.get_summary () |> Result.iter ~f:(fun res -> Stdio.print_endline res)
+let rec serve_loop ~handler =
+  match In_channel.input_line In_channel.stdin with
+  | Some line ->
+    (match handler line with
+     | Some response ->
+       Out_channel.output_line Out_channel.stdout response;
+       Out_channel.flush Out_channel.stdout
+     | None -> ());
+    serve_loop ~handler
+  | None -> ()
+;;
+
+let () =
+  let server_info = { name = "timew-mcp"; version = "1.0.0" } in
+  let capabilities = { tools = { list_changed = true }; resources = () } in
+  let my_handler (msg : string) : string option =
+    try
+      let message = Yojson.Safe.from_string msg in
+      handle_message message ~server_info ~capabilities
+    with
+    | Yojson.Json_error err ->
+      let error_response : server_error_response =
+        { jsonrpc = rpc_version
+        ; id = None
+        ; error =
+            { code = -32700 (* Parse error *)
+            ; message = "Failed to parse message: " ^ err
+            }
+        }
+      in
+      Some (yojson_of_server_error_response error_response |> Yojson.Safe.to_string)
+    | exn ->
+      let id =
+        try Some (Yojson.Safe.from_string msg |> get_id) with
+        | _ -> None
+      in
+      let error_response : server_error_response =
+        { jsonrpc = rpc_version
+        ; id
+        ; error =
+            { code = -32603 (* Internal error *)
+            ; message = "Internal error: " ^ Exn.to_string exn
+            }
+        }
+      in
+      Some (yojson_of_server_error_response error_response |> Yojson.Safe.to_string)
+  in
+  serve_loop ~handler:my_handler
+;;
